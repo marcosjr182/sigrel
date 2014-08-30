7c79b9fdb51222d5efd0f70d9a325061ad6d342f
Marcos Siqueira Jr <junior.crvg@gmail.com>
2014-08-30 14:59:27 -0300

    Creates Activity class

---
+++ activity.rb
@@ -0,0 +1,12 @@
+class Activity
+
+	attr_accessor :name
+
+  def initialize (name, date,time,description)
+    @name = name
+    @date = date
+    @time = time
+    @description = description
+  end
+
+end
